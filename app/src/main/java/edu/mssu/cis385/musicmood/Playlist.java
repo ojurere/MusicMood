4428f716a3aa33a233875e4e663c5bc878d60d6c
ojurere <58527680+ojurere@users.noreply.github.com>
2020-12-01 23:20:05 -0600

    Changed icon launcher and added array list for songName and songArtist in Song.java.

--- app/src/main/java/edu/mssu/cis385/musicmood/Playlist.java
+++ app/src/main/java/edu/mssu/cis385/musicmood/Playlist.java
@@ -8,12 +8,5 @@ public class Playlist {
 
 
 
-    public static void main(String[] args) {
-        ArrayList<String> cars = new ArrayList<String>();
-        cars.add("Volvo");
-        cars.add("BMW");
-        cars.add("Ford");
-        cars.add("Mazda");
-        System.out.println(cars);
-    }
+
 }
